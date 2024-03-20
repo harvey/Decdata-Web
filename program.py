aee7189abfbbf4426168e52711636d05509952aa
Harvey Chandler <67712051+hifrens@users.noreply.github.com>
2024-03-20 10:31:04 +0000

    Made more readable code & better server debug

--- program.py
+++ program.py
@@ -1,6 +1,4 @@
-import time
 from decimal import Decimal # This used for making the yields not read as 0.00000 instead of xE-10
-
 
 # This is the path to the ndx file (The data file)
 pathToFile = "P107JAICRP_38_3_Nuclear_Decay_Data_suppl_data/P 107 JAICRP 38(3) Nuclear Decay Data for Dosimetric Calculations(supplementary data)"
@@ -9,9 +7,7 @@ pathToFile = "P107JAICRP_38_3_Nuclear_Decay_Data_suppl_data/P 107 JAICRP 38(3) N
 with open(pathToFile + '/ICRP-07.NDX') as f:
     lines = f.readlines()
 
-
 # CLASS FOR OBJECTS IN obj
-
 class RadioNuclide():
     '''Radionuclide class for ICRP 107 Publication'''
     def __init__(self, name: str, halflife: str, unit: str, decaymethod: str, daughters: dict):
@@ -33,34 +29,34 @@ class RadioNuclide():
         self.daughters = daughterDict
 
 # 'Getter' to return the name of the Radionuclide
-def getName(RN):
+def getName(RN: object):
     '''Returns the name of the RadioNuclide from the objs list.
     objs[index].name can also be used to achieve this.'''
     return RN.name
 
 # 'Getter' to return the half life of the Radionuclide
-def getHalfLife(RN):
+def getHalfLife(RN: object):
     '''Returns the half-life of the RadioNuclide from the objs list.
     objs[index].halflife can also be used to achieve this.'''
     return RN.halflife
 
-def getUnit(RN):
+def getUnit(RN: object):
     '''Returns the unit for the half-life of the RadioNuclide from the objs list.
     objs[index].unit can also be used to achieve this.'''
     return RN.unit
 
-def getDecayMethod(RN):
+def getDecayMethod(RN: object):
     '''Returns the decay method for the RadioNuclide from the objs list.
     objs[index].decaymethod can also be used to achieve this.'''
     return RN.decaymethod
 
-def getDaughters(RN):
+def getDaughters(RN: object):
     '''Returns the dictionary of daughters for the RadioNuclide from the objs list.
     objs[index].daughters can also be used to achieve this.'''
     return RN.daughters
 
-def searchFor(RN, f="r"):                   #f = format -- search for Name,HalfLife,Amount of Daughters, etc. 
-    #                                  'write binary search here WHERE RN=theValue and f=FORMAT' # FORMAT WAS NEVER USED.
+def searchFor(RN: str, f="r"):                   #f = format -- search for Name,HalfLife,Amount of Daughters, etc. 
+    #                                               write binary search here WHERE RN=theValue and f=FORMAT' # FORMAT WAS NEVER USED.
     '''This gets the index of the item in the objs list for which the input of the function is the name of a RadioNuclide.
     e.g. searchFor('Ar-37') will return 53, as objs[53].name is Ar-37.'''
     left = 0                            # -- left pointer
@@ -82,13 +78,13 @@ def searchFor(RN, f="r"):                   #f = format -- search for Name,HalfL
     stable = RadioNuclide(RN, 'Stable', '', '', {})
     #print(getName(objs[mid]))
     objs.insert(mid, stable)
-    objs.sort(key=lambda x: x.name)
+    objs.sort(key=lambda x: x.name) #sort list by name just incase 'mid' is not the correct place to put in the list.
     return mid
 # return last item in list
 
 
 
-def unitTo(RN, newUnit):                             #usage:   RN = 'name' of a Radionuclide. e.g. "Ac-232".
+def unitTo(RN: str, newUnit):                             #usage:   RN = 'name' of a Radionuclide. e.g. "Ac-232".
     'change RN (original value) to newUnit'
     'e.g. h ---> d:   RN/24 turns from h to d'
     
@@ -126,7 +122,7 @@ def unitTo(RN, newUnit):                             #usage:   RN = 'name' of a 
     
     return val
     
-def getContains(elementName):
+def getContains(elementName: str):
     '''Returns a string (formatted as a list) which returns all of the isotopes of an element.'''
     output = ""
     for i in range(len(objs)-1):
@@ -138,12 +134,12 @@ def getContains(elementName):
     return output
 
 
-def standard_form_to_superscript(s): 
+def standard_form_to_superscript(s: str): 
     '''Replaces the +'s and -'s of an input to superscript variations of them. (This is used for formatting with the frontend)'''
     output = s.replace('-', '⁻').replace('+', '⁺')
     return output
 
-def get_decay_chain(RN, yield_multiplier=1.0, depth=0, visited=None):
+def get_decay_chain(RN: object, yield_multiplier=1.0 , depth=0, visited=None):
     '''Return the decay chain of a given element.
     Formatted with ':::' for seperating each element, and +'s represent depth.'''
     if visited is None:
@@ -166,7 +162,8 @@ def get_decay_chain(RN, yield_multiplier=1.0, depth=0, visited=None):
 
     return decayChain
 
-def getradioactivedecay_chain(RN):
+def getradioactivedecay_chain(RN: str):
+    '''Returns the b64 image of the decay chain, given a radionuclide name as a string (Uses radioactivedecay library).'''
     import radioactivedecay as rd       # ONLY IMPORT WHEN YOU NEED IT SO DOESN'T 
     import matplotlib.pyplot as plt     # USE UP TOO MUCH SYSTEM MEMORY FOR NOTHING.
     import io
@@ -187,14 +184,22 @@ def getradioactivedecay_chain(RN):
 # Create the objects in the class (Radionuclides)
 objs = list() # Create a list called objs (objects) to store all the Radionuclides
 
-for i in range(len(lines)-1): # Loops as many times as there are lines in the ndx file             
-                                                                                                              ##########                                                                                                     ###############                                                                                                     ##################                                                                                                      ###############
-    #objs.append(RadioNuclide(lines[i+1][:7].replace(" ", ""), lines[i+1][7:15].replace(" ", ""), lines[i+1][15:17].replace(" ", ""), lines[i+1][17:25].replace(" ", ""),  {lines[i+1][62:66].replace(" ", "") + " " + lines[i+1][53:60].replace(" ", ""):lines[i+1][66:77].replace(" ", ""), lines[i+1][87:91].replace(" ", "") + " " + lines[i+1][78:85].replace(" ", ""):lines[i+1][92:102].replace(" ", ""), lines[i+1][112:116].replace(" ", "") + " " + lines[i+1][103:110].replace(" ", ""):lines[i+1][117:127].replace(" ", ""), lines[i+1][137:141].replace(" ", "") + " " + lines[i+1][128:135].replace(" ", ""):lines[i+1][142:152].replace(" ", "")})) 
-    objs.append(RadioNuclide(lines[i+1][:7].replace(" ", ""), lines[i+1][7:15].replace(" ", ""), lines[i+1][15:17].replace(" ", ""), lines[i+1][17:25].replace(" ", ""),  {lines[i+1][53:60].replace(" ", ""):lines[i+1][66:77].replace(" ", ""), lines[i+1][78:85].replace(" ", ""):lines[i+1][92:102].replace(" ", ""), lines[i+1][103:110].replace(" ", ""):lines[i+1][117:127].replace(" ", ""), lines[i+1][128:135].replace(" ", ""):lines[i+1][142:152].replace(" ", "")})) 
+for i in range(1, len(lines)-1): # Loops as many times as there are lines in the ndx file
+    objName =           lines[i][:7].replace(" ", "")
+    objHalfLife =       lines[i][7:15].replace(" ", "")
+    objUnit =           lines[i][15:17].replace(" ", "")
+    objDecayMethod =    lines[i][17:25].replace(" ", "")
+    objDaughter1 =      lines[i][53:60].replace(" ", "")
+    objYield1 =         lines[i][66:77].replace(" ", "")
+    objDaughter2 =      lines[i][78:85].replace(" ", "")
+    objYield2 =         lines[i][92:102].replace(" ", "")
+    objDaughter3 =      lines[i][103:110].replace(" ", "")
+    objYield3 =         lines[i][117:127].replace(" ", "")
+    objDaughter4 =      lines[i][128:135].replace(" ", "")
+    objYield4 =         lines[i][142:152].replace(" ", "")
+    
+    objs.append(RadioNuclide(objName, objHalfLife, objUnit, objDecayMethod,  {objDaughter1:objYield1, objDaughter2:objYield2, objDaughter3:objYield3, objDaughter4:objYield4})) 
     # Add (or append) a new item 
-                                                                                                                                                        # 62 - 66 (was 67:77)
-#                             #chars from#                       #chars from#                         #chars from#
-#                                0 to 7                             7 to 15                             15 to 17
-#                               (name)                            (half-life)                          (unit)                                (decay method)                #lineNumForDaughter1 + " " + #daughters (1)                          #yield (1)                                                            #lineNumForDaughter1 + " " +  #daughters (2)                          #yield (2)                                                                                   #daughters (3)                          #yield (3)                                                                      #daughters (4)                              #yield (4)                    
 
 print('Imported template file successfully.')
+#This is called when there are no errors in the program.
--- serv2.py
+++ serv2.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from waitress import serve
+
 from program import getName
 from program import getHalfLife
 from program import getUnit
@@ -8,7 +9,6 @@ from program import getContains
 from program import getDecayMethod
 from program import get_decay_chain
 from program import getradioactivedecay_chain
-
 from program import searchFor
 
 from program import objs
@@ -17,16 +17,17 @@ app = Flask(__name__)
 cors = CORS(app)
 
 @app.route("/receiver", methods=["POST"])
-def postME(): 
+def postME():
+   b64request = False
    data = request.get_json()
-   print(str(data)) # -- > so we can work out the command later
+   # str(data) is the full command
+
    output = ""
 
    if data.get('Search'):
-      print('Search')
+      print('Search: [{search}]'.format(search=data.get('Search')))
       isotopes = getContains(data.get('Search'))
       temp = isotopes.split()
-      print(temp)
       methods = ""
       halflife = ""
       unit = ""
@@ -36,24 +37,21 @@ def postME():
          halflife = halflife + getHalfLife(objs[index]) + ","
          unit = unit         + getUnit(objs[index]) + ","
          
+      #Remove final commas.
       methods = methods[:-1]
       halflife = halflife[:-1]
       unit = unit[:-1]
       
-   
       output = "{ \"isotopes\":\"" + isotopes + "\", \"decaymethods\":\"" + methods + "\", \"halflife\":\"" + halflife + "\", \"unit\":\"" + unit + "\"}"
 
-
    elif data.get('Info'):
-      print('info')
-      print(searchFor((data.get('Info')), 'r'))
+      print('Info: [{info}]'.format(info=data.get('Info')))
       output = "{ \"HalfLife\":\"" + getHalfLife(objs[searchFor(str(data.get('Info')).replace(" ",""))]) + "\"}"
    
    elif data.get('Chain'):   
-      print('chain')
+      print('CSS Chain: [{chain}]'.format(chain=data.get('Chain')))
       chainStr = ""
       chain = get_decay_chain(objs[searchFor(str(data.get('Chain')).replace(" ",""))])
-      print(f'chain={chain}')
       for i in range(len(chain)):
          chainStr = chainStr + str(chain[i]) + ":::"
 
@@ -61,11 +59,16 @@ def postME():
    
 
    elif data.get('ChainV2'):
-      print('chainV2')
+      b64request = True
+      print('Image Chain: [{chainV2}]'.format(chainV2=data.get('ChainV2')))
       b64 = getradioactivedecay_chain(data.get('ChainV2'))
       output = "{ \"b64\":\"" + b64 + "\"}"
 
-
+   if not b64request:
+      print(f'Output: [{output}]\n')
+   else:
+      print(f'Output: [b64 image (if issues change b64request to False)]\n')
+      
    return(output)
    
 if __name__ == "__main__":
