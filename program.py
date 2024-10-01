f0b49e842e3681b30b0c1f71d241c4e2a06731a8
Harvey Chandler <67712051+hifrens@users.noreply.github.com>
2024-10-01 10:00:05 +0100

    Class attributes now private & improved concistency

--- program.py
+++ program.py
@@ -11,10 +11,10 @@ with open(pathToFile + '/ICRP-07.NDX') as f:
 class RadioNuclide():
     '''Radionuclide class for ICRP 107 Publication'''
     def __init__(self, name: str, halflife: str, unit: str, decaymethod: str, daughters: dict):
-        self.name = name
-        self.halflife = halflife
-        self.unit = unit
-        self.decaymethod = decaymethod
+        self.__name = name
+        self.__halflife = halflife
+        self.__unit = unit
+        self.__decaymethod = decaymethod
         
         ## difficult so keep seperate.
 
@@ -26,7 +26,27 @@ class RadioNuclide():
                 daughterDict.update({str(key).split(" ")[0]:value})
         
         
-        self.daughters = daughterDict
+        self.__daughters = daughterDict
+
+    def get_name(self):
+        '''Returns the name of an object'''
+        return self.__name
+    
+    def get_halflife(self):
+        '''Returns the half-life of an object'''
+        return self.__halflife
+
+    def get_unit(self):
+        '''Returns the unit of an object'''
+        return self.__unit
+    
+    def get_decaymethod(self):
+        '''Returns the decay method of an object'''
+        return self.__decaymethod
+    
+    def get_daughters(self):
+        '''Returns the daughters of an object'''
+        return self.__daughters
 
     def get_decay_chain(self, yield_multiplier=1.0 , depth=0, visited=None):
         '''Return the decay chain of a given element.
@@ -35,15 +55,15 @@ class RadioNuclide():
             visited = set()
 
         decayChain = []
-        decayChain.append([depth * "+" + self.name + " ,HalfLife: " + standard_form_to_superscript(self.halflife) + " " + self.unit + ",Yield: " + standard_form_to_superscript(str('%.3E' % Decimal(yield_multiplier)))])  # Adjusted indentation
-        daughters = self.daughters
+        decayChain.append([depth * "+" + self.get_name() + " ,HalfLife: " + standard_form_to_superscript(self.get_halflife()) + " " + self.get_unit() + ",Yield: " + standard_form_to_superscript(str('%.3E' % Decimal(yield_multiplier)))])  # Adjusted indentation
+        daughters = self.get_daughters()
 
         for key, value in daughters.items():
             daughter = objs[searchFor(key)] 
             yield_value = float(value) * yield_multiplier
 
-            if daughter.name not in visited:
-                visited.add(daughter.name)
+            if daughter.get_name() not in visited:
+                visited.add(daughter.get_name())
                 decayChain.extend(daughter.get_decay_chain(yield_value, depth + 1, None))
 
         if depth == 0:
@@ -51,14 +71,14 @@ class RadioNuclide():
 
         return decayChain
 
-    def getradioactivedecay_chain(self):
+    def get_radioactivedecay_chain(self):
         '''Returns the b64 image of the decay chain, given a radionuclide name as a string (Uses radioactivedecay library).'''
         import radioactivedecay as rd       # ONLY IMPORT WHEN YOU NEED IT SO DOESN'T 
         import matplotlib.pyplot as plt     # USE UP TOO MUCH SYSTEM MEMORY FOR NOTHING.
         import io
         import base64
 
-        nuc = rd.Nuclide(self.name)
+        nuc = rd.Nuclide(self.get_name())
         nuc.plot()
 
         buffer = io.BytesIO()
@@ -69,6 +89,45 @@ class RadioNuclide():
         b64Image = base64.b64encode(buffer.getvalue()).decode('utf-8')
 
         return(b64Image)
+    
+    def unit_to(self, newUnit):                             #usage:   RN = 'name' of a Radionuclide. e.g. "Ac-232".
+        'change RN (original value) to newUnit'
+        'e.g. h ---> d:   RN/24 turns from h to d'
+        
+        index = objs[searchFor(self.get_name,"name")]
+        val = float(index.get_halflife())
+
+        originalUnit = index.get_unit()
+
+        # First I will convert all values into seconds
+        if originalUnit == 'm':
+            val = val * 60
+        elif originalUnit == 'h':
+            val = val * 3600
+        elif originalUnit == 'd':
+            val = val * 86400
+        elif originalUnit == 'y':
+            val = val * 31536000
+        elif originalUnit == 'u':
+            val = val / 1000000
+        elif originalUnit == 'ms':
+            val = val / 1000
+        
+        if newUnit == 'm':
+            val = val / 60
+        elif newUnit == 'h':
+            val = val / 3600
+        elif newUnit == 'd':
+            val = val / 86400
+        elif newUnit == 'y':
+            val = val / 31536000
+        elif newUnit == 'u':
+            val = val * 1000000
+        elif newUnit == 'ms':
+            val = val * 1000
+        
+        return val
+
 
 def searchFor(RN: str, f="r"):                   #f = format -- search for Name,HalfLife,Amount of Daughters, etc. 
     #                                               write binary search here WHERE RN=theValue and f=FORMAT' # FORMAT WAS NEVER USED.
@@ -80,7 +139,7 @@ def searchFor(RN: str, f="r"):                   #f = format -- search for Name,
     while left <= right:
         mid = (left + right) // 2                                   # might implement caching algorithm for real time searches
         #print(mid)
-        mid_obj = objs[mid].name
+        mid_obj = objs[mid].get_name()
 
         if mid_obj == RN:
             return mid
@@ -93,51 +152,13 @@ def searchFor(RN: str, f="r"):                   #f = format -- search for Name,
     stable = RadioNuclide(RN, 'Stable', '', '', {})
     #print(getName(objs[mid]))
     objs.insert(mid, stable)
-    objs.sort(key=lambda x: x.name) #sort list by name just incase 'mid' is not the correct place to put in the list.
+    objs.sort(key=lambda x: x.get_name()) #sort list by name just incase 'mid' is not the correct place to put in the list.
     return mid
 # return last item in list
-
-def unitTo(RN: str, newUnit):                             #usage:   RN = 'name' of a Radionuclide. e.g. "Ac-232".
-    'change RN (original value) to newUnit'
-    'e.g. h ---> d:   RN/24 turns from h to d'
-    
-    index = objs[searchFor(RN,"name")]
-    val = float(index.halflife)
-
-    originalUnit = index.unit
-
-    # First I will convert all values into seconds
-    if originalUnit == 'm':
-        val = val * 60
-    elif originalUnit == 'h':
-        val = val * 3600
-    elif originalUnit == 'd':
-        val = val * 86400
-    elif originalUnit == 'y':
-        val = val * 31536000
-    elif originalUnit == 'u':
-        val = val / 1000000
-    elif originalUnit == 'ms':
-        val = val / 1000
-    
-    if newUnit == 'm':
-        val = val / 60
-    elif newUnit == 'h':
-        val = val / 3600
-    elif newUnit == 'd':
-        val = val / 86400
-    elif newUnit == 'y':
-        val = val / 31536000
-    elif newUnit == 'u':
-        val = val * 1000000
-    elif newUnit == 'ms':
-        val = val * 1000
-    
-    return val
     
 def getContains(elementName: str):
     '''Returns a string (formatted as a list) which returns all of the isotopes of an element.'''
-    isotopes = [obj.name for obj in objs if obj.name.startswith(elementName + '-')]
+    isotopes = [obj.get_name() for obj in objs if obj.get_name().startswith(elementName + '-')]
     return ', '.join(isotopes)
 
 def standard_form_to_superscript(s: str): 
--- serv2.py
+++ serv2.py
@@ -2,10 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from waitress import serve
 
-from program import getContains
-from program import searchFor
-
-from program import objs
+from program import *
 
 import json
 
@@ -39,9 +36,9 @@ def postME():
       unit = ""
       for i in range(len(temp)):
          index = searchFor(str(temp[i]).replace(",",""))
-         methods = methods   + objs[index].decaymethod + ","
-         halflife = halflife + objs[index].halflife + ","
-         unit = unit         + objs[index].unit + ","
+         methods = methods   + objs[index].get_decaymethod() + ","
+         halflife = halflife + objs[index].get_halflife() + ","
+         unit = unit         + objs[index].get_unit() + ","
          
       #Remove final commas.
       methods = methods[:-1]
@@ -52,7 +49,7 @@ def postME():
 
    elif data.get('Info'):
       print('Info: [{info}]'.format(info=data.get('Info')))
-      output = "{ \"HalfLife\":\"" + objs[searchFor(str(data.get('Info')).replace(" ",""))].halflife + "\"}"
+      output = "{ \"HalfLife\":\"" + objs[searchFor(str(data.get('Info')).replace(" ",""))].get_halflife() + "\"}"
    
    elif data.get('Chain'):   
       print('CSS Chain: [{chain}]'.format(chain=data.get('Chain')))
@@ -67,7 +64,7 @@ def postME():
    elif data.get('ChainV2'):
       b64request = True
       print('Image Chain: [{chainV2}]'.format(chainV2=data.get('ChainV2')))
-      b64 = objs[searchFor(str(data.get('ChainV2')).replace(" ",""))].getradioactivedecay_chain()
+      b64 = objs[searchFor(str(data.get('ChainV2')).replace(" ",""))].get_radioactivedecay_chain()
       output = "{ \"b64\":\"" + b64 + "\"}"
 
    if not b64request:
